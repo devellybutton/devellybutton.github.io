e3b2c51a43be91174a7b905d3d720c6d4d4ee9b1
devellybutton <leegr0106@gmail.com>
2025-05-12 17:27:20 +0900

    vite.config.js 수정

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,15 @@
-export default {
-  base: "/",
-};
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "/devellybutton.github.io/",
+  build: {
+    outDir: "..",
+    minify: "terser",
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+  },
+});
